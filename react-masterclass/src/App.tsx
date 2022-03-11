f1f61693d2ead8d741c1f6072b09181e17bfd9f0
jayhug2 <zero900r@gmail.com>
2022-03-12 00:34:48 +0900

    react and typescript study

---
+++ react-masterclass/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="black"/>
+      <Circle bgColor="tomato" text="I'm here"/>
+    </div>
+  );
+}
+
+export default App;
